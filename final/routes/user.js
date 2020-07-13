438ef117843e0aff863458bb03f4e9ced1e2a330
Pragya <pragz01@gmail.com>
2020-07-14 03:50:19 +0530

    final changes - 3

--- final/routes/user.js
+++ final/routes/user.js
@@ -4,16 +4,6 @@ const router = express.Router()
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
 const { userById, read, update, purchaseHistory } = require('../controllers/user')
 
-<<<<<<< HEAD
-=======
-
-// router.get('/secret', requireSignin, (req, res) => {
-//     res.json({
-//         user: 'got here'
-//     });
-// }); 
-
->>>>>>> 854e0cb7e81dc464867aa8ee083c6962ef026122
 router.get('/user/:userId', requireSignin, isAuth, read)
 router.put('/user/:userId', requireSignin, isAuth, update)
 router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
